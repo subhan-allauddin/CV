2e050d98f61ec2ffca1b839b112710a2d2b18e2d
Your GitHub Username <Your GitHub Email>
2025-05-04 00:34:06 +0500

    changes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import Image from "next/image";
 import  Navbar  from "@/components/Navbar/Navbar";
 import Portfolio from "@/components/Portfolio/Portfolio"
 import CaseStudies from "@/components/CaseStudies/CaseStudies";
-import Testimonials from "@/components/Testimonials/Testimonials"
 import Footer from "@/components/Footer/Footer";
 import { BentoGridDemo } from "@/components/BentoGrid/BentoGrid";
-import TimelineDemo from "@/components/Timeline/TimelineDemo";
+
 
 
 
@@ -17,9 +16,6 @@ export default function Home() {
          <Portfolio />
          <CaseStudies />
          <BentoGridDemo  />
-         {/* <TimelineDemo /> */}
-         {/* <Testimonials /> */}
-        
          <Footer />
        </div>
 
